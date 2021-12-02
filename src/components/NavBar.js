28d2bbeeced60bbec76386e8dec7e2e93cc391ad
Nancy Borges <nabg1504@gmail.com>
2021-12-01 23:06:37 -0300

    trabajo 3er clase

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    return (
+        <div class="row"> <nav>
+            <div class="columna coll"><h2>Core Uruguay</h2>
+            </div>
+            <div class="columna"> <img src="https://wallpaper.sc/es/ipad/wp-content/uploads/2018/08/ipad-2048x2048-thumbnail_01411-256x256.jpg" alt="logo" height="100px" width="100px"/></div>
+            <div class="columna columna2" > 
+            <a href="#" class="link">Ingresar/Registrarse</a>
+            <a href="#" class="link">Novedades</a>
+            <a href="#" class="link">Productos</a>
+            
+            </div></nav>
+            
+            
+        </div>
+
+    )
+}
+
+
+export default Navbar
